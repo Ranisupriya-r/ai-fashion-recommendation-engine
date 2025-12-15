9177a4191bbb9ab8c80623956e7be88e6f7fa85a
Ranisupriya-r <71452235+Ranisupriya-r@users.noreply.github.com>
2025-12-15 10:59:12 -0800

    Implement image preprocessing function
    Added a function to preprocess images for MobileNetV2 and CLIP.

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,1 +1,11 @@
+import tensorflow as tf
+from PIL import Image
+import numpy as np
+
+def preprocess_image(image_path):
+    """Prepare an image for MobileNetV2 and CLIP processing."""
+    img = Image.open(image_path).resize((224, 224))
+    img_array = tf.keras.preprocessing.image.img_to_array(img)
+    img_array = tf.keras.applications.mobilenet_v2.preprocess_input(img_array)
+    return np.expand_dims(img_array, axis=0)
 
